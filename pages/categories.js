c4b188a1a8fece8376ae6f38f7a385c18709b1c6
Charbel Sonon <charbelsnn@gmail.com>
2023-12-01 08:29:29 +0100

    now can see all categoris on categories page and can each subcategory with its own single view page

--- pages/categories.js
+++ pages/categories.js
@@ -24,6 +24,23 @@ const CategoryGrid = styled.div`
 `;
 
 const CategoryTitle = styled.div`
+display: flex;
+justify-content: center; 
+margin-top: 10px;
+margin-bottom: 0;
+align-items: center;
+gap: 10px;
+  
+  h2{
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+  a{
+    color:#555;
+    display: inline-block;
+  }
+`;
+const SubCategoryTitle = styled.div`
   display:flex;
   margin-top: 10px;
   margin-bottom: 0;
@@ -53,50 +70,175 @@ const ShowAllSquare = styled(Link)`
   text-decoration: none;
 `;
 
-export default function CategoriesPage({maincategories,categoriesProducts,wishedProducts=[]})
-{
+// export default function CategoriesPage({maincategories,categoriesProducts,subCategories,subCategoriesProducts,wishedProducts=[]})
+// {
 
-    return(
-        <>
-            <Header />
-            <Center>
-             {maincategories.map(cat=>(
-                <CategoryWrapper  key={cat._id}>
-                    <CategoryTitle>
-                    <h2>{cat.name}</h2>
-                    <div>
-                        <Link href={'/category/'+cat._id}>Voir tout</Link>
-                    </div>
-                    </CategoryTitle>
-                    <CategoryGrid>
-                    {categoriesProducts[cat._id].map((p,index) => (
-                <RevealWrapper delay={index*50}>
-                  <ProductBox {...p} wished={wishedProducts.includes(p._id)} />
-                </RevealWrapper>
-              ))}
-              <RevealWrapper delay={categoriesProducts[cat._id].length*50}>
-                <ShowAllSquare href={'/category/'+cat._id}>
-                  Voir tout &rarr;
-                </ShowAllSquare>
-              </RevealWrapper>
+//     return(
+//         <>
+//             <Header />
+//             <Center>
+//              {/* {maincategories.map(cat=>(
+//                 <CategoryWrapper  key={cat._id}>
+//                     <CategoryTitle>
+//                     <h2>{cat.name}</h2>
+//                     <div>
+//                         <Link href={'/category/'+cat._id}>Voir tout</Link>
+//                     </div>
+//                     </CategoryTitle>
+//                     <CategoryGrid>
+//                     {categoriesProducts[cat._id].map((p,index) => (
+//                 <RevealWrapper delay={index*50}>
+//                   <ProductBox {...p} wished={wishedProducts.includes(p._id)} />
+//                 </RevealWrapper>
+//               ))}
+//               <RevealWrapper delay={categoriesProducts[cat._id].length*50}>
+//                 <ShowAllSquare href={'/category/'+cat._id}>
+//                   Voir tout &rarr;
+//                 </ShowAllSquare>
+//               </RevealWrapper>
                        
-                    </CategoryGrid>
+//                     </CategoryGrid>
                    
-                </CategoryWrapper>
-             ))}
+//                 </CategoryWrapper>
+//              ))} */}
+//              {subCategories.map(cat=>(
+//               <>
+//                 {subCategoriesProducts[cat._id].length > 0 && (
 
-            </Center>
-        </>
-    )
+// <CategoryWrapper  key={cat._id}>
+// <CategoryTitle>
+// <h2>{cat.name}</h2>
+// <div>
+//     <Link href={'/category/'+cat._id}>Voir tout</Link>
+// </div>
+// </CategoryTitle>
+// <CategoryGrid>
+
+// {subCategoriesProducts[cat._id].map((p,index) => (
+// <RevealWrapper delay={index*50}>
+// <ProductBox {...p} wished={wishedProducts.includes(p._id)} />
+// </RevealWrapper>
+// ))}
+// <RevealWrapper delay={subCategoriesProducts[cat._id].length*50}>
+// <ShowAllSquare href={'/category/'+cat._id}>
+// Voir tout &rarr;
+// </ShowAllSquare>
+// </RevealWrapper>
+   
+// </CategoryGrid>
+
+// </CategoryWrapper>
+
+//                 )}
+//               </>
+              
+//              ))}
+
+//             </Center>
+//         </>
+//     )
+// }
+
+
+export default function CategoriesPage({
+  mainCategories,
+  categoriesProducts,
+  subCategories,
+  subCategoriesProducts,
+  wishedProducts = [],
+}) {
+  return (
+    <>
+      {/* En-tête */}
+      <Header />
+      <Center>
+        {mainCategories.map((mainCat) => (
+          <CategoryWrapper key={mainCat._id}>
+            <CategoryTitle>
+              <h2>{mainCat.name}</h2>
+             
+            </CategoryTitle>
+            
+           
+
+            {/* Affichage des sous-catégories pour la catégorie parente */}
+            {subCategories
+              .filter((subCat) => subCat.parent.toString() === mainCat._id.toString())
+              .map((subCat) => (
+                <>
+                      {subCategoriesProducts[subCat._id].length > 0 && (
+
+<CategoryWrapper key={subCat._id}>
+<SubCategoryTitle>
+  <h2>{subCat.name}</h2>
+  <div>
+    <Link href={"/category/" + subCat._id}>Voir tout</Link>
+  </div>
+</SubCategoryTitle>
+<CategoryGrid>
+  {subCategoriesProducts[subCat._id].map((p, index) => (
+    <RevealWrapper delay={index * 50} key={p._id}>
+      <ProductBox {...p} wished={wishedProducts.includes(p._id)} />
+    </RevealWrapper>
+  ))}
+  <RevealWrapper delay={subCategoriesProducts[subCat._id].length * 50}>
+    <ShowAllSquare href={"/category/" + subCat._id}>
+      Voir tout &rarr;
+    </ShowAllSquare>
+  </RevealWrapper>
+</CategoryGrid>
+</CategoryWrapper>
+                      )}
+
+
+                      
+
+                    
+                </>
+           
+              ))}
+
+{subCategories
+              .filter((subCat) => subCat.parent.toString() === mainCat._id.toString()).length <=0 &&(
+                            <CategoryGrid>
+                                                 {categoriesProducts[mainCat._id].map((p,index) => (
+                                            <RevealWrapper delay={index*50}>
+                                              <ProductBox {...p} wished={wishedProducts.includes(p._id)} />
+                                            </RevealWrapper>
+                                          ))}
+                                          <RevealWrapper delay={categoriesProducts[mainCat._id].length*50}>
+                                            <ShowAllSquare href={'/category/'+mainCat._id}>
+                                              Voir tout &rarr;
+                                            </ShowAllSquare>
+                                          </RevealWrapper>
+                                                   
+                                                </CategoryGrid>
+                      ) }
+          </CategoryWrapper>
+        ))}
+      </Center>
+    </>
+  );
 }
+
 
 //fonction pour recupérer toutes les catégories sant parent et les produits de la liste de souhait
 export async function getServerSideProps(ctx) {
   await mongooseConnect();
   const categories = await Category.find();
   const mainCategories = categories.filter(c => !c.parent);
+  const subCategories = categories.filter(c => c.parent);
   const categoriesProducts = {}; // catId => [products]
+  const subCategoriesProducts = {}; // catId => [products]
   const allFetchedProductsId = [];
+  
+  for (const subCat of subCategories)
+  {
+    const subCatId= subCat._id.toString();
+    subCategoriesProducts[subCat._id] = await Product.find({ category: subCatId }, null, {limit:3,sort:{'_id':-1}});
+
+  }
+
   for (const mainCat of mainCategories) {
     const mainCatId = mainCat._id.toString();
     const childCatIds = categories
@@ -119,10 +261,15 @@ export async function getServerSideProps(ctx) {
 
   return {
     props: {
-      maincategories: JSON.parse(
+      mainCategories: JSON.parse(
         JSON.stringify(mainCategories)
       ),
+      subCategories: JSON.parse(
+        JSON.stringify(subCategories)
+      ),
+      
       categoriesProducts: JSON.parse(JSON.stringify(categoriesProducts)),
+      subCategoriesProducts: JSON.parse(JSON.stringify(subCategoriesProducts)),
       wishedProducts: wishedProducts.map(i => i.product.toString()),
     },
   };
--- pages/category/[id].js
+++ pages/category/[id].js
@@ -44,16 +44,144 @@ const Filter = styled.div`
   }
 `;
 
-export default function CategoryPage({category,subCategories,products:originalProducts})
+// export default function CategoryPage({category,subCategories,products:originalProducts})
+// {
+//     //valeurs par défaut de certains états
+//     const defaultSorting = '_id-desc';
+    // const defaultFilterValues = category.properties
+    // .map(p => ({name:p.name,value:'all'}));
+
+//     // État local pour gérer les produits, les filtres, la charge et le tri
+//     const [products,setProducts] = useState(originalProducts);
+//     const [filtersValues,setFiltersValues] = useState(defaultFilterValues);
+//     const [sort,setSort] = useState(defaultSorting);
+//     const [loadingProducts,setLoadingProducts] = useState(false);
+//     const [filtersChanged,setFiltersChanged] = useState(false);
+
+//     // Gestionnaire de changement de filtre
+//     function handleFilterChange(filterName, filterValue) {
+//         // Met à jour les valeurs des filtres
+//         setFiltersValues(prev => {
+//         return prev.map(p => ({
+//             name:p.name,
+//             value: p.name === filterName ? filterValue : p.value,
+//         }));
+//         });
+//         setFiltersChanged(true);
+//     }
+
+//     // Effet pour charger les produits en fonction des filtres et du tri
+//     useEffect(()=>{
+//         if (!filtersChanged) {
+//             return;
+//         }
+//         setLoadingProducts(true);
+//         const catIds = [category._id, ...(subCategories?.map(c => c._id) || [])];
+//         const params = new URLSearchParams;
+//         params.set('categories', catIds.join(','));
+//         params.set('sort', sort);
+//         filtersValues.forEach(f => {
+//             if (f.value !== 'all') {
+//             params.set(f.name, f.value);
+//             }
+//         });
+//         const url = `/api/products?` + params.toString();
+//         axios.get(url).then(res => {
+//             setProducts(res.data);
+//             setLoadingProducts(false);
+//         })
+
+//     },[filtersValues, sort, filtersChanged])
+
+//     // Rendu de la page individuelle pour chaque produit avec les produits et les filtres
+//     return(
+//         <>
+//         <Header />
+//         <Center>
+//             <CategoryHeader>
+//                  {/* ... (affichage du titre de la catégorie) */}
+//                 <h1>{category.name}</h1>
+//                 <FiltersWrapper>
+//                     {/* ... (affichage des filtres pour la catégorie) */}
+//                     {category.properties.map(prop =>(
+//                         <Filter key={prop.name}>
+//                             {prop.name}
+//                             <select
+//                              onChange={ev => handleFilterChange(prop.name, ev.target.value)}
+//                              value={filtersValues.find(f => f.name === prop.name).value}>
+//                                 <option value="all">Tout</option>
+//                                 {prop.values.map(val =>(
+//                                     <option key={val} value={val}>{val}</option>  
+//                                 ))}
+//                             </select>
+//                         </Filter>
+//                     ))}
+//                      <Filter>
+//               <span>Trier:</span>
+//               <select
+//                 value={sort}
+//                 onChange={ev => {
+//                   setSort(ev.target.value);
+//                   setFiltersChanged(true);
+//                 }}>
+//                 <option value="price-asc">Prix croissant</option>
+//                 <option value="price-desc">Prix décroissant</option>
+//                 <option value="_id-desc">Nouveaux produits</option>
+//                 <option value="_id-asc">Les plus anciens</option>
+//               </select>
+//             </Filter>
+//                 </FiltersWrapper>
+//             </CategoryHeader>
+
+//           {/* Affichage du spinner pendant le chargement */}  
+//         {loadingProducts && (
+//           <Spinner fullWidth />
+//         )}
+
+//          {/* Affichage des produits ou du message si aucun produit n'est trouvé */}
+//         {!loadingProducts && (
+//           <div>
+//             {products.length > 0 && (
+//               <ProductsGrid products={products} />
+//             )}
+//             {products.length === 0 && (
+//               <div>Désolé, aucun produit trouvé</div>
+//             )}
+//           </div>
+//         )}
+//         </Center>
+//         </>
+//     )
+// }
+
+export default function CategoryPage({category,parentCat,products:originalProducts})
 {
+
+
     //valeurs par défaut de certains états
     const defaultSorting = '_id-desc';
-    const defaultFilterValues = category.properties
-    .map(p => ({name:p.name,value:'all'}));
+
+let mergedValues=[]
+    
+    const defaultFilterValues = category.properties.map(p => ({ name: p.name, value: 'all' }));
+    if(parentCat?.properties)
+    {
+      const defaultParentFilterValues = parentCat.properties.map(p => ({ name: p.name, value: 'all' })) ;
+      mergedValues = [...defaultFilterValues, ...defaultParentFilterValues].map(p => ({ name: p.name, value: 'all' }));
+    }
+    else
+    {
+      mergedValues = defaultFilterValues;
+    }
+    
+
+
+console.log(mergedValues);
+
 
     // État local pour gérer les produits, les filtres, la charge et le tri
     const [products,setProducts] = useState(originalProducts);
-    const [filtersValues,setFiltersValues] = useState(defaultFilterValues);
+    const [filtersValues,setFiltersValues] = useState(mergedValues);
     const [sort,setSort] = useState(defaultSorting);
     const [loadingProducts,setLoadingProducts] = useState(false);
     const [filtersChanged,setFiltersChanged] = useState(false);
@@ -76,7 +204,7 @@ export default function CategoryPage({category,subCategories,products:originalPr
             return;
         }
         setLoadingProducts(true);
-        const catIds = [category._id, ...(subCategories?.map(c => c._id) || [])];
+        const catIds = [category._id];
         const params = new URLSearchParams;
         params.set('categories', catIds.join(','));
         params.set('sort', sort);
@@ -104,6 +232,19 @@ export default function CategoryPage({category,subCategories,products:originalPr
                 <FiltersWrapper>
                     {/* ... (affichage des filtres pour la catégorie) */}
                     {category.properties.map(prop =>(
+                        <Filter key={prop.name}>
+                            {prop.name}
+                            <select
+                             onChange={ev => handleFilterChange(prop.name, ev.target.value)}
+                             value={filtersValues.find(f => f.name === prop.name).value}>
+                                <option value="all">Tout</option>
+                                {prop.values.map(val =>(
+                                    <option key={val} value={val}>{val}</option>  
+                                ))}
+                            </select>
+                        </Filter>
+                    ))}
+                    {parentCat?.properties?.map(prop =>(
                         <Filter key={prop.name}>
                             {prop.name}
                             <select
@@ -155,17 +296,36 @@ export default function CategoryPage({category,subCategories,products:originalPr
 }
 
 
+// // Récupère les données côté serveur pour la page de la catégorie
+// export async function getServerSideProps(context) {
+//     // Récupère la catégorie, ses sous-catégories et les produits associés
+//     const category = await Category.findById(context.query.id);
+//     const subCategories = await Category.find({parent:category._id});
+//     const catIds = [category._id, ...subCategories.map(c => c._id)];
+//     const products = await Product.find({category:catIds});
+//     return {
+//       props:{
+//         category: JSON.parse(JSON.stringify(category)),
+//         subCategories: JSON.parse(JSON.stringify(subCategories)),
+//         products: JSON.parse(JSON.stringify(products)),
+//       }
+//     };
+//   }
 // Récupère les données côté serveur pour la page de la catégorie
 export async function getServerSideProps(context) {
     // Récupère la catégorie, ses sous-catégories et les produits associés
     const category = await Category.findById(context.query.id);
-    const subCategories = await Category.find({parent:category._id});
-    const catIds = [category._id, ...subCategories.map(c => c._id)];
+    
+    const parentCategorie = await Category.findById(category.parent)
+
+    const catIds = [category._id];
     const products = await Product.find({category:catIds});
+    // const properties = [category.properties, parentCategorie.properties]
+    console.log( JSON.parse(JSON.stringify(parentCategorie)))
     return {
       props:{
         category: JSON.parse(JSON.stringify(category)),
-        subCategories: JSON.parse(JSON.stringify(subCategories)),
+        parentCat: JSON.parse(JSON.stringify(parentCategorie)),
         products: JSON.parse(JSON.stringify(products)),
       }
     };
