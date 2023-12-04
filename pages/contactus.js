573ffa99274d9e6c87a4e8648a2056e02614e2e8
Charbel Sonon <charbelsnn@gmail.com>
2023-12-04 13:24:07 +0100

    adding mail sending

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,6 +1,8 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
 
 const ContactContainer = styled.div`
   margin: 20px;
@@ -49,10 +51,37 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 
-export default function ContactUs() {
-  const handleSubmit = (e) => {
+export default  function ContactUs() {
+    const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [message, setMessage] = useState('')
+const [phone, setPhone] = useState('')
+const [submitted, setSubmitted] = useState(false)
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Ajouter ici la logique pour traiter le formulaire de contact
+  console.log('Sending')
+let data = {
+    name,
+    email,
+    phone,
+    message
+  }
+   
+   
+    try {
+       const response = axios.post('/api/contact',data)
+       console.log(response)
+     
+        if (response.code!==200) {
+          throw new Error(`Invalid response: ${response.status}`);
+        }
+        alert('Merci de nous avoir contacter! Nous vous répondrons sous peu!');
+        console.log('Response succeeded!')
+        setSubmitted(true)
+      } catch (err) {
+        console.error(err);
+        alert("Nous ne pouvons pas envoyer votre email pour le moment, réessayer plus tard?");
+      }
   };
 
   return (
@@ -63,15 +92,19 @@ export default function ContactUs() {
         <ContactForm onSubmit={handleSubmit}>
           <FormGroup>
             <Label htmlFor="name">Nom</Label>
-            <Input type="text" id="name" name="name" />
+            <Input type="text" id="name" name="name" onChange={(e)=>{setName(e.target.value)}} required />
           </FormGroup>
           <FormGroup>
             <Label htmlFor="email">Adresse email</Label>
-            <Input type="email" id="email" name="email" />
+            <Input type="email" id="email" name="email" onChange={(e)=>{setEmail(e.target.value)}} required />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="phone">Numéro de Téléphone</Label>
+            <Input type="text" id="phone" name="phone" onChange={(e)=>{setPhone(e.target.value)}}  />
           </FormGroup>
           <FormGroup>
             <Label htmlFor="message">Message</Label>
-            <TextArea id="message" name="message" rows="5" />
+            <TextArea id="message" name="message" rows="5" onChange={(e)=>{setMessage(e.target.value)}} required />
           </FormGroup>
           <SubmitButton type="submit">Envoyer</SubmitButton>
         </ContactForm>
